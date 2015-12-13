7fef8beef5514a513a216ae35456e383d3e3f387
nidefawl <devnull@localhost>
2015-12-13 03:37:29 +0100

    New file

---
+++ src/nidefawl/qubes/block/BlockCategory.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package nidefawl.qubes.block;
+
+/**
+ * @author Michael Hept 2015
+ * Copyright: Michael Hept
+ */
+public enum BlockCategory {
+    GROUND, ROCK, LEAVES, STONE, LOG, FLOWER, VINE, PLANT, UNASSIGNED  
+}
