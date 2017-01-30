37921c53676ecb45e7845cc8b30ef4f828503bef
nidefawl <devnull@localhost>
2017-01-30 15:23:56 +0100

    Temporal AA code

--- src/nidefawl/qubes/render/post/SMAA.java
+++ src/nidefawl/qubes/render/post/SMAA.java
@@ -31,8 +31,10 @@ public class SMAA {
 
     public Shader       shaderAAEdge;
     public Shader       shaderCopyTexture;
+    public Shader       shaderDrawAlphaChannel;
     public Shader       shaderAABlendWeight;
     public Shader       shaderAANeighborBlend;
+    public Shader       shaderTemporalResolve;
     private FrameBuffer fbFlipInput;
     private FrameBuffer fbAAEdge;
     private FrameBuffer fbAAWeightBlend;
@@ -43,6 +45,12 @@ public class SMAA {
 
     private boolean usePredicatedThresholding;
 
+    private boolean useReprojection;
+
+    private FrameBuffer[] fbOutput = new FrameBuffer[2];
+    int curBuffer=0;
+    int prevBuffer=1;
+
     
     public final static int SMAA_PRESET_LOW = 0;
     public final static int SMAA_PRESET_MEDIUM = 1;
@@ -51,12 +59,13 @@ public class SMAA {
     final static String[] qualDefines = { "SMAA_PRESET_LOW", "SMAA_PRESET_MEDIUM", "SMAA_PRESET_HIGH", "SMAA_PRESET_ULTRA" };
     public static String[] qualDesc = { "Low", "Medium", "High", "Ultra" };
     public SMAA(final int quality) {
-        this(quality, false, false);
+        this(quality, false, false, false);
     }
-    public SMAA(final int quality, final boolean usePredTex, boolean srgb) {
-        System.out.println("SMAA init");
+    public SMAA(final int quality, final boolean usePredTex, boolean srgb, final boolean reprojection) {
+        this.useReprojection = reprojection;
         this.srgb = srgb;
         this.usePredicatedThresholding = usePredTex;
+        Engine.TEMPORAL_OFFSET = useReprojection;
         AssetManager assetMgr = AssetManager.getInstance();
         AssetBinary areaTexData = assetMgr.loadBin("textures/areatex.bin");
         AssetBinary searchTexData = assetMgr.loadBin("textures/searchtex.bin");
@@ -71,6 +80,9 @@ public class SMAA {
                 if ("SMAA_PREDICATION".equals(define)) {
                     return "#define SMAA_PREDICATION "+(usePredTex?"1":"0");
                 }
+                if ("SMAA_REPROJECTION".equals(define)) {
+                    return "#define SMAA_REPROJECTION "+(reprojection?"1":"0");
+                }
                 return null;
             }
         };
@@ -81,18 +93,27 @@ public class SMAA {
             Shader new_AAEdge;
             Shader new_BlendWeight;
             Shader new_neighbor_blend;
+            Shader new_temporal_resolve = null;
+            shaderDrawAlphaChannel = assetMgr.loadShader(mgr, "debug/drawAlphaChannel", "screen_scaled_quad", null, null, def);
             new_CopyTexture = assetMgr.loadShader(mgr, "post/SMAA/copytexture", def);
             new_AAEdge = assetMgr.loadShader(mgr, "post/SMAA/SMAA_edgedetection", def);
             new_BlendWeight = assetMgr.loadShader(mgr, "post/SMAA/SMAA_blend_weight", def);
             new_neighbor_blend = assetMgr.loadShader(mgr, "post/SMAA/SMAA_neighbor_blend", def);
+            if (useReprojection) {
+                new_temporal_resolve = assetMgr.loadShader(mgr, "post/SMAA/SMAA_temporal_resolve", def);
+            }
             shaderCopyTexture = new_CopyTexture;
             shaderAAEdge = new_AAEdge;
             shaderAABlendWeight = new_BlendWeight;
             shaderAANeighborBlend = new_neighbor_blend;
+            shaderTemporalResolve = new_temporal_resolve;
             shaderCopyTexture.enable();
             shaderCopyTexture.setProgramUniform1i("texColor", 0);
             if (usePredTex) {
-                shaderCopyTexture.setProgramUniform1i("texMaterial", 1);    
+                shaderCopyTexture.setProgramUniform1i("texMaterial", 1);
+            }
+            if (useReprojection) {
+                shaderCopyTexture.setProgramUniform1i("velocityTex", 2);
             }
             shaderAAEdge.enable();
             shaderAAEdge.setProgramUniform1i("texColor", 0);
@@ -103,9 +124,17 @@ public class SMAA {
             shaderAABlendWeight.setProgramUniform1i("edgesTex", 0);
             shaderAABlendWeight.setProgramUniform1i("areaTex", 1);
             shaderAABlendWeight.setProgramUniform1i("searchTex", 2);
+            shaderAABlendWeight.setProgramUniform4f("jitterOffset", 0, 0, 0, 0);
             shaderAANeighborBlend.enable();
             shaderAANeighborBlend.setProgramUniform1i("texColor", 0);
             shaderAANeighborBlend.setProgramUniform1i("blendTex", 1);
+            shaderAANeighborBlend.setProgramUniform1i("velocityTex", 2);
+            if (useReprojection) {
+                shaderTemporalResolve.enable();
+                shaderTemporalResolve.setProgramUniform1i("texColorCurrent", 0);
+                shaderTemporalResolve.setProgramUniform1i("texColorPrev", 1);
+                shaderTemporalResolve.setProgramUniform1i("velocityTex", 2);
+            }
             Shader.disable();
         } catch (ShaderCompileError e) {
             e.printStackTrace();
@@ -122,14 +151,25 @@ public class SMAA {
         this.fbFlipInput.setColorAtt(GL_COLOR_ATTACHMENT0, format);
         this.fbFlipInput.setFilter(GL_COLOR_ATTACHMENT0, GL_LINEAR, GL_LINEAR);
         this.fbFlipInput.setClearColor(GL_COLOR_ATTACHMENT0, 0F, 0F, 0F, 0F);
-        if (this.usePredicatedThresholding) {
-            this.fbFlipInput.setColorAtt(GL_COLOR_ATTACHMENT1, GL_R16F);
-            this.fbFlipInput.setFilter(GL_COLOR_ATTACHMENT1, GL_NEAREST, GL_NEAREST);
-            this.fbFlipInput.setClearColor(GL_COLOR_ATTACHMENT1, 0F, 0F, 0F, 0F);
-        }
+        this.fbFlipInput.setColorAtt(GL_COLOR_ATTACHMENT1, GL_R16F);
+        this.fbFlipInput.setFilter(GL_COLOR_ATTACHMENT1, GL_NEAREST, GL_NEAREST);
+        this.fbFlipInput.setClearColor(GL_COLOR_ATTACHMENT1, 0F, 0F, 0F, 0F);
+        this.fbFlipInput.setColorAtt(GL_COLOR_ATTACHMENT2, GL_RG16F);
+        this.fbFlipInput.setFilter(GL_COLOR_ATTACHMENT2, GL_NEAREST, GL_NEAREST);
+        this.fbFlipInput.setClearColor(GL_COLOR_ATTACHMENT2, 0F, 0F, 0F, 0F);
         this.fbFlipInput.setup(mgr);
         this.fbAAEdge = FrameBuffer.make(mgr, displayWidth, displayHeight, format, true, true);
         this.fbAAWeightBlend = FrameBuffer.make(mgr, displayWidth, displayHeight, format, true, true);
+        if (this.useReprojection) {
+            for (int i = 0; i < 2; i++) {
+                FrameBuffer fb = new FrameBuffer(displayWidth, displayHeight);
+                fb.setColorAtt(GL_COLOR_ATTACHMENT0, format);
+                fb.setFilter(GL_COLOR_ATTACHMENT0, GL_LINEAR, GL_LINEAR);
+                fb.setClearColor(GL_COLOR_ATTACHMENT0, 0F, 0F, 0F, 0F);
+                fb.setup(mgr);
+                this.fbOutput[i] = fb;
+            }
+        }
         FrameBuffer.unbindFramebuffer();
     }
     
@@ -173,7 +213,7 @@ public class SMAA {
      * @param texture
      * @param finalTarget 
      */
-    public void render(int texture, int material, int debugTexture, FrameBuffer finalTarget) {
+    public void render(int texture, int material, int velocity, int debugTexture, FrameBuffer finalTarget) {
         if (true||Game.GL_ERROR_CHECKS) {
             boolean b = glGetBoolean(GL_DEPTH_TEST);
             if (!b) {
@@ -192,7 +232,7 @@ public class SMAA {
             }
            
         }
-
+        
         Engine.updateOrthoMatrix(Game.displayWidth, Game.displayHeight, true);
         UniformBuffer.updateOrtho();
         fbFlipInput.bind();
@@ -200,10 +240,13 @@ public class SMAA {
         if (this.usePredicatedThresholding) {
             GL.bindTexture(GL_TEXTURE1, GL_TEXTURE_2D, material);
         }
+        if (this.useReprojection) {
+            GL.bindTexture(GL_TEXTURE2, GL_TEXTURE_2D, velocity);
+        }
         shaderCopyTexture.enable();
         Engine.drawFSTri();
         if (debugTexture != 1) {
-            renderSMAA(fbFlipInput.getTexture(0), usePredicatedThresholding?fbFlipInput.getTexture(1):0, debugTexture, finalTarget);
+            renderSMAA(fbFlipInput.getTexture(0), usePredicatedThresholding?fbFlipInput.getTexture(1):0, useReprojection?fbFlipInput.getTexture(2):0, velocity, debugTexture, finalTarget);
         }
         Engine.updateOrthoMatrix(Game.displayWidth, Game.displayHeight, false);
         UniformBuffer.updateOrtho();
@@ -226,15 +269,22 @@ public class SMAA {
                     break;
                 case 3:
                     Shaders.textured_to_srgb.enable();
+//                    shaderDrawAlphaChannel.enable();
+//                    shaderDrawAlphaChannel.setProgramUniform1i("texColor", 0);
                     tex = fbAAWeightBlend.getTexture(0);
                     break;
             }
+            glClear(GL11.GL_DEPTH_BUFFER_BIT);
             GL.bindTexture(GL_TEXTURE0, GL_TEXTURE_2D, tex);
+            
             Engine.drawFSQuad(1);
         }
     }
-    public void renderSMAA(int texture, int material, int debugTexture, FrameBuffer finalTarget) {
-        
+    public void renderSMAA(int texture, int material, int velocityInverted, int velocity, int debugTexture, FrameBuffer finalTarget) {
+        if (useReprojection) {
+            prevBuffer = curBuffer;
+            curBuffer = (curBuffer+1) % 2;
+        }
         
         fbAAEdge.bind();
         fbAAEdge.clearFrameBuffer();
@@ -249,13 +299,19 @@ public class SMAA {
         }
         fbAAWeightBlend.bind();
         fbAAWeightBlend.clearFrameBuffer();
-        
         //copy over the depth buffer
         fbAAEdge.bindRead();
         GL30.glBlitFramebuffer(0, 0, fbAAEdge.getWidth(), fbAAEdge.getHeight(), 0, 0, fbAAWeightBlend.getWidth(), fbAAWeightBlend.getHeight(), GL_DEPTH_BUFFER_BIT, GL_NEAREST);
         FrameBuffer.unbindReadFramebuffer();
         
         shaderAABlendWeight.enable();
+        if (useReprojection) {
+            int n = Engine.getTemporalJitterIdx();
+            if (n%2==0)
+                shaderAABlendWeight.setProgramUniform4f("jitterOffset", 1, 1, 1, 0);
+            else 
+                shaderAABlendWeight.setProgramUniform4f("jitterOffset", 2, 2, 2, 0);
+        }
         GL.bindTexture(GL_TEXTURE0, GL_TEXTURE_2D, fbAAEdge.getTexture(0));
         GL.bindTexture(GL_TEXTURE1, GL_TEXTURE_2D, this.areaTex);
         GL.bindTexture(GL_TEXTURE2, GL_TEXTURE_2D, this.searchTex);
@@ -266,15 +322,36 @@ public class SMAA {
         if (debugTexture == 2) {
             return;
         }
-        if (finalTarget == null) FrameBuffer.unbindFramebuffer();
-        else {
-            finalTarget.bind();
-            finalTarget.clearFrameBuffer();
+        if (useReprojection) {
+            this.fbOutput[curBuffer].bind();
+        } else {
+            if (finalTarget == null) FrameBuffer.unbindFramebuffer();
+            else {
+                finalTarget.bind();
+                finalTarget.clearFrameBuffer();
+            }
         }
         shaderAANeighborBlend.enable();
         GL.bindTexture(GL_TEXTURE0, GL_TEXTURE_2D, texture);
         GL.bindTexture(GL_TEXTURE1, GL_TEXTURE_2D, fbAAWeightBlend.getTexture(0));
+        if (this.useReprojection) {
+            GL.bindTexture(GL_TEXTURE2, GL_TEXTURE_2D, velocityInverted);
+        }
         Engine.drawFSTri();
+        if (useReprojection) {
+            GL.bindTexture(GL_TEXTURE0, GL_TEXTURE_2D, this.fbOutput[curBuffer].getTexture(0));
+            GL.bindTexture(GL_TEXTURE1, GL_TEXTURE_2D, this.fbOutput[prevBuffer].getTexture(0));
+            if (this.useReprojection) {
+                GL.bindTexture(GL_TEXTURE2, GL_TEXTURE_2D, velocity);
+            }
+            shaderTemporalResolve.enable();
+            if (finalTarget == null) FrameBuffer.unbindFramebuffer();
+            else {
+                finalTarget.bind();
+                finalTarget.clearFrameBuffer();
+            }
+            Engine.drawFSTri();
+        } 
     }
 
 
